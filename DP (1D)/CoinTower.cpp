7e861ea33d3d4cb5ec80965706e7a7e366327d8b
Amiya Ranjan <66031190+Sunny955@users.noreply.github.com>
2021-05-20 15:42:09 +0530

    Updated CoinTower.cpp

--- DP (1D)/CoinTower.cpp
+++ DP (1D)/CoinTower.cpp
@@ -29,6 +29,55 @@ Sample Output :
 #include <vector>
 using namespace std;
 
+/* Iterative Solution 
+
+string findWinner(int n, int x, int y)
+{
+	// Write your code here .
+    string out;
+    int *dp=new int[n+1]();
+    
+    dp[0]=100;
+    dp[1]=1;
+    dp[x]=1;
+    dp[y]=1;
+    
+    for(int i=2;i<=n;i++)
+    {
+        int ans1=-1,ans2=-1,ans3=-1,ans;
+        if(dp[i]==0)
+        {
+            if(i-1>0)
+            {
+                ans1=dp[i-1]^1;
+            }
+            if(i-x>0)
+            {
+                ans2=dp[i-x]^1;
+            }
+            if(i-y>0)
+            {
+                ans3=dp[i-y]^1;
+            }
+        }
+        
+    	ans=max(ans1,max(ans2,ans3));
+        dp[i]=ans;
+        
+    }
+    
+    if(dp[n]!=0)
+    {
+        out="Beerus";
+    }
+    else
+    {
+       out="Whis";    
+    }
+    return out;
+}
+*/
+
 int BeerusWin(vector<int> &dp,int n,int x,int y){
     if(n==1 || n==x || n==y)
         return 1;
